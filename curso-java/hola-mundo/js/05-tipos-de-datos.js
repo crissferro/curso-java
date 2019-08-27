536dc8c32e299ae564feeaaf48f043e6c07f750f
Cristian Ferro <crissferro@gmail.com>
2019-08-27 17:16:59 -0300

    operadores yt tipo de datos

---
+++ curso-java/hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+
+alert("El resultado de la Sumatoria es: " + operacion);
+
+
+
+//tipos de datos
+
+//entero, string, decimal, booleano, array y objetos
+
+var numero_entero = 44;
+var cadena_texto = "hola entre comillas";
+var verdadero_o_falso = true;
+
+var numero_falso = "33";
+
+
+// console.log(Number(numero_falso) + 7);
+
+console.log(String(numero_falso) + 7);
